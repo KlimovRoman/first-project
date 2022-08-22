43e53e3f02511a6ee08ba619ab5e0ee36a1b449a
klimov.roman.vit <klimov.roman.vit@yandex.ru>
2022-08-22 16:43:41 +0300

    мы на проде

--- converter.java
+++ converter.java
@@ -7,3 +7,24 @@ ssdsd
 2- git commit
 3- git push
 4- 
+
+# вывести список созданных ключей:
+ls -la .ssh/
+
+# для более структурированного вывода вы можете добавить команде пару параметров:
+ls -lAFho .ssh/ 
+
+
+    ssh-keygen -t ed25519 -C "practicum.student@yandex.ru"
+    
+
+    git config --global user.name "Username" 
+# Имя или ник нужно написать латиницей и в кавычках.
+
+git config --global user.email username@yandex.ru
+# Здесь нужно указать свой настоящий e-mail.
+
+
+git config --list
+
+ХУЙ!!!
